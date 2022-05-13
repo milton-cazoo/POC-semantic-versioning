a57e2c5991036d7e8e7c782940920ac654722f40
Milton Boos Junior <ext_milton.junior@cazoo.co.uk>
2022-05-13 18:21:46 +0100

    fix: update branches config

--- release.config.js
+++ release.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-    branches: ['master'],
-    tagFormat: '${version}',
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        [
-            '@semantic-release/npm',
-            {
-                pkgRoot: 'build'
-            }
-        ],
-        [
-            '@semantic-release/exec',
-            {
-                prepareCmd:
-                    'cd .. && npm --no-git-tag-version version ${nextRelease.version}'
-            }
-        ],
-        '@semantic-release/git',
-        '@semantic-release/github'
-    ]
+  branches: ["main"],
+  tagFormat: "${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        pkgRoot: "build",
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd:
+          "cd .. && npm --no-git-tag-version version ${nextRelease.version}",
+      },
+    ],
+    "@semantic-release/git",
+    "@semantic-release/github",
+  ],
 };
